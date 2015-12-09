aab6096ce2810fd80f421eef3dce7c5425d4eb02
zflan <lanzf2011@gmail.com>
2015-12-09 13:45:55 +0800

    add log4js
    add log4js

---
+++ routes/logger.js
@@ -0,0 +1,21 @@
+var log4js = require('log4js');
+
+log4js.configure({
+	appenders:[
+	{type: "console"},//日志输出到控制台
+	{
+		type:'file',
+		filename:'logs/access.log', //日志输出到logs下的access.log文件
+		maxLogSize:1024,
+		backups:4,
+		category:'normal'
+	}
+	],
+	replaceConsole:true
+});
+exports.logger = function(name){
+	var logger = log4js.getlogger(name);
+	logger.setLevel('DEBUG');
+	return logger;
+}
+exports.log4js = log4js;

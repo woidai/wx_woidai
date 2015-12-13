c2c75761845aa9ea59c9c31188ba6bcb5296f07b
zflan <lanzf2011@gmail.com>
2015-12-13 17:55:45 +0800

    remove useless code
    remove useless code

--- routes/wechat_pm.js
+++ routes/wechat_pm.js
@@ -29,99 +29,14 @@ exports.callback = function (req, res) {
   });
 };
 
-exports.reply = wechat(config.mp, wechat.text(function (message, req, res) {
-  console.log(message);
-  var input = (message.Content || '').trim();
-
-  if (input === 'login') {
-    res.reply([{
-      title: '登陆页面',
-      description: '去登陆',
-      picurl: config.domain + '/assets/qrcode.jpg',
-      url: config.domain + '/login'
-    }]);
-    return;
-  }
-
-  if (input === '大王') {
-    return res.reply("不要叫我大王，要叫我女王大人啊……");
-  }
-  if (input.length < 2) {
-    return res.reply('内容太少，请多输入一点:)');
-  }
-  var data = alpha.search(input);
-  var content = '';
-  switch (data.status) {
-  case 'TOO_MATCHED':
-    content = '找到API过多，请精确一点：\n' + data.result.join(', ').substring(0, 100) + '...';
-    break;
-  case 'MATCHED':
-    content = data.result.map(function (item) {
-      var replaced = (item.desc || '')
-        .replace(/<p>/ig, '').replace(/<\/p>/ig, '')
-        .replace(/<code>/ig, '').replace(/<\/code>/ig, '')
-        .replace(/<pre>/ig, '').replace(/<\/pre>/ig, '')
-        .replace(/<strong>/ig, '').replace(/<\/strong>/ig, '')
-        .replace(/<ul>/ig, '').replace(/<\/ul>/ig, '')
-        .replace(/<li>/ig, '').replace(/<\/li>/ig, '')
-        .replace(/<em>/ig, '').replace(/<\/em>/ig, '')
-        .replace(/&#39;/ig, "'");
-
-      return {
-        title: item.path,
-        description: item.textRaw + ':\n' + replaced,
-        picurl: config.domain + '/assets/qrcode.jpg',
-        url: config.domain + '/detail?id=' + item.hash
-      };
-    });
-    if (data.more && data.more.length) {
-      content.push({
-        title: '更多：' + data.more.join(', ').substring(0, 200) + '...',
-        description: data.more.join(', ').substring(0, 200) + '...',
-        picurl: config.domain + '/assets/qrcode.jpg',
-        url: config.domain + '/404'
-      });
-    }
-    break;
-  default:
-    res.wait('view');
-    return;
-    break;
-  }
-  var from = message.FromUserName;
-  if (!Array.isArray(content)) {
-    if (from === 'oPKu7jgOibOA-De4u8J2RuNKpZRw') {
-      content = '主人你好：\n' + content;
-    }
-    if (from === 'oPKu7jpSY1tD1xoyXtECiM3VXzdU') {
-      content = '女王大人:\n' + content;
-    }
-  }
-  console.log(content);
-  res.reply(content);
-}).image(function (message, req, res) {
-  console.log(message);
-  res.reply('还没想好图片怎么处理啦。');
-}).location(function (message, req, res) {
-  console.log(message);
-  res.reply('想和我约会吗，不要的啦。');
-}).voice(function (message, req, res) {
-  console.log(message);
-  res.reply('心情不好，不想搭理你。');
-}).link(function (message, req, res) {
-  console.log(message);
-  res.reply('点连接进来的是吧！');
-}).event(function (message, req, res) {
-  console.log(message);
-  if (message.Event === 'subscribe') {
-    // 用户添加时候的消息
-    res.reply('谢谢添加Node.js公共帐号:)\n回复Node.js API相关关键词，将会得到相关描述。如：module, setTimeout等');
-  } else if (message.Event === 'unsubscribe') {
-    res.reply('Bye!');
-  } else {
-    res.reply('暂未支持! Coming soon!');
-  }
-}));
+exports.reply = wechat(config.mp, function (req, res, next) {
+   var message = req.weixin;
+  if (message.FromUserName === 'hi') {
+    // 回复屌丝(普通回复)
+    res.reply('呵呵');
+  } 
+  
+});
 
 var tpl = ejs.compile(fs.readFileSync(path.join(VIEW_DIR, 'detail.html'), 'utf-8'));
 exports.detail = function (req, res) {
